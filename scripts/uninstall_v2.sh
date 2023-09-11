0462865255237db0f672c8f796fd5615ee2aa448
Deepak Chourasia <dchouras@redhat.com>
2023-09-11 18:23:31 +0530

    adding new scripts

--- scripts/uninstall_v2.sh
+++ scripts/uninstall_v2.sh
@@ -6,3 +6,4 @@ oc delete subscription $(oc get subscription -n redhat-ods-operator --no-headers
 oc delete operatorgroup $(oc get operatorgroup -n redhat-ods-operator --no-headers | awk '{print $1}') -n redhat-ods-operator
 oc delete ns -l opendatahub.io/generated-namespace
 oc delete namespace redhat-ods-operator
+oc new-project redhat-ods-operator
