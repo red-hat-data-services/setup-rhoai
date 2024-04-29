5a6620f070d33e820b60994492616a33b35860a1
Deepak Chourasia <dchouras@redhat.com>
2024-04-29 12:55:00 +0530

    updating the script

--- setup_rhoai.sh
+++ setup_rhoai.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#scripts/./uninstall-rhoai.sh
+#scripts/./uninstall-rhoai.sh -t operator
 scripts/./add_brew_pull_secret.sh
 oc apply -f config/imagepolicy.yaml
 oc apply -f config/catalogsource.yaml
