6ff2abf218af2c55a2ba9f79d18fe468ef8cbb20
Rishab Prasad <riprasad@redhat.com>
2024-08-05 14:26:55 +0530

    Add comments and ensure script compatibility with macOS

--- scripts/add_brew_pull_secret.sh
+++ scripts/add_brew_pull_secret.sh
@@ -1,9 +1,33 @@
-secrets=$(mktemp -d)
-oc extract secret/pull-secret -n openshift-config --to="${secrets}"
-if ! grep brew.registry.redhat.io < "${secrets}"/.dockerconfigjson
-then
-  echo adding brew pull secret
-  sed -i 's/^{"auths":{/{"auths":{"brew.registry.redhat.io":{"auth":"'$(cat ~/.ssh/.brew_token)'"},/' "${secrets}"/.dockerconfigjson
-  oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson="${secrets}"/.dockerconfigjson
+#!/bin/bash
+
+# used gsed for MacOS
+if [[ "$(uname)" == "Darwin" ]]; then
+  if ! command -v gsed &>/dev/null; then
+      echo "gsed is not installed. Please install it using 'brew install gnu-sed'."
+      exit 1
+  fi
+  sed_command="gsed"
+else
+  sed_command="sed"
 fi
+
+
+# Create a temporary directory to store extracted secrets
+secrets=$(mktemp -d)
+
+# Extract the pull secret from OpenShift configuration
+oc extract secret/pull-secret -n openshift-config --to="${secrets}"
+
+# Check if the brew registry is already included in the pull secret
+if ! grep -q 'brew.registry.redhat.io' "${secrets}/.dockerconfigjson"; then
+  echo "Adding brew pull secret..."
+
+  # Update the pull secret with brew registry credentials
+  ${sed_command} -i 's/^{"auths":{/{"auths":{"brew.registry.redhat.io":{"auth":"'$(cat ~/.ssh/.brew_token)'"},/' "${secrets}"/.dockerconfigjson
+
+  # Update the pull secret in OpenShift configuration
+  oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson="${secrets}/.dockerconfigjson"
+fi
+
+# Clean up temporary directory
 rm -rf "${secrets}"
--- upgrade-rhods.sh
+++ upgrade-rhods.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# used gsed for MacOS
+if [[ "$(uname)" == "Darwin" ]]; then
+  if ! command -v gsed &>/dev/null; then
+      echo "gsed is not installed. Please install it using 'brew install gnu-sed'."
+      exit 1
+  fi
+  sed_command="gsed"
+else
+  sed_command="sed"
+fi
 
 wget https://rhods-devops.s3.amazonaws.com/IBM/WatsonX/latest_images
 image=$(cat latest_images)
-sed -i 's/image: */image: '$image'/g' config/catalogsource.yaml
+${sed_command} -i 's/image: */image: '$image'/g' config/catalogsource.yaml
