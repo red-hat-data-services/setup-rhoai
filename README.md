d9b746ccadf950b51d25f8e05dc90f326676c2ef
Deepak Chourasia <dchouras@redhat.com>
2024-11-06 10:37:16 +0530

    syncing the secret file name with tracer

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 * Make sure you have the "oc" cli installed
 * Make sure you are logged in to the cluster from the cli as an admin
 * Uninstall Openshift AI completely if it is installed
-* Store the quay token in a file at this location - ~/.ssh/.quay_ro_token
+* Store the quay token in a file at this location - ~/.ssh/.rhoai_quay_ro_token
 * Update the required index image in config/catalogsource.yaml as the value of "spec.image" field
 * Run setup_rhoai.sh
--- konflux/add_quay_pull_secret.sh
+++ konflux/add_quay_pull_secret.sh
@@ -3,7 +3,7 @@ oc extract secret/pull-secret -n openshift-config --to="${secrets}"
 if ! grep 'quay.io/rhoai' < "${secrets}"/.dockerconfigjson
 then
   echo adding quay pull secret
-  sed -i 's/^{"auths":{/{"auths":{"quay.io\/rhoai":{"auth":"'$(cat ~/.ssh/.quay_ro_token)'"},/' "${secrets}"/.dockerconfigjson
+  sed -i 's/^{"auths":{/{"auths":{"quay.io\/rhoai":{"auth":"'$(cat ~/.ssh/.rhoai_quay_ro_token)'"},/' "${secrets}"/.dockerconfigjson
   oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson="${secrets}"/.dockerconfigjson
 fi
 #rm -rf "${secrets}"
